dd509b84c6a3fafc20a41112f595e410cc956bc0
DerelDaga <xasiel.gallegos@gmail.com>
2026-05-18 19:01:58 +0000

    18 may 2026, 13:01

--- js/config.js
+++ js/config.js
@@ -1,15 +1,17 @@
-/**
- * SIGOD - Configuración Global
- */
+
+
 const Config = {
-    // Microsoft Entra ID (Azure AD) - Para inicio de sesión
     MSAL: {
         CLIENT_ID: "831e8f41-1d3a-4048-bf74-b6f4ff7d08b3",
-        // El Authority se forma usando tu TENANT_ID
-        AUTHORITY: "https://login.live.com/oauth20_remoteconnect.srf",
+        AUTHORITY: "https://login.microsoftonline.com/consumers",
         REDIRECT_URI: window.location.origin,
-        SCOPES: ["openid", "profile", "offline_access", "User.Read", "Mail.Read", "Mail.ReadWrite"]
-        // Nota: CLIENT_SECRET no se utiliza en el frontend por seguridad. 
-        // MSAL.js utiliza PKCE para flujos de navegador.
+        SCOPES: [
+            "openid",
+            "profile",
+            "offline_access",
+            "User.Read",
+            "Mail.Read",
+            "Mail.ReadWrite"
+        ]
     }
 };
