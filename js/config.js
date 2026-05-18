3cebfb4b0f33acac67adae6231862dadaef75418
DerelDaga <xasiel.gallegos@gmail.com>
2026-05-18 18:29:06 +0000

    18 may 2026, 12:29

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const Config = {
     MSAL: {
         CLIENT_ID: "831e8f41-1d3a-4048-bf74-b6f4ff7d08b3",
         // El Authority se forma usando tu TENANT_ID
-        AUTHORITY: "https://www.microsoft.com/link",
+        AUTHORITY: "https://login.live.com/oauth20_remoteconnect.srf",
         REDIRECT_URI: window.location.origin,
         SCOPES: ["openid", "profile", "offline_access", "User.Read", "Mail.Read", "Mail.ReadWrite"]
         // Nota: CLIENT_SECRET no se utiliza en el frontend por seguridad. 
